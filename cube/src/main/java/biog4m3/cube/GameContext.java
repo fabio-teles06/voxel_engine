809bfbed9f44ffed97eb16eb0b62d0dd0a7eb5ce
biog4m3 <biog4m3ryt@gmail.com>
2025-04-08 15:00:04 -0300

    Refactor Cube class and introduce GameContext for game state management

---
+++ cube/src/main/java/biog4m3/cube/GameContext.java
@@ -0,0 +1,9 @@
+package biog4m3.cube;
+
+public class GameContext {
+    public int tickCount = 0;
+    public long lifeTime = 0;
+
+    public int currentFPS = 0;
+    public int currentTPS = 0;
+}
